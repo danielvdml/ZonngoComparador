0f8bf6fc8f95b368fbada5d2ffb848fb2a6f256f
Victor Daniel <danielvdmlfiis@gmail.com>
2016-03-06 12:44:54 -0500

    Agregando buscador con autcompletado

--- js/app/directives/directives.js
+++ js/app/directives/directives.js
@@ -19,19 +19,7 @@
 			restrict: 'E',
 			templateUrl:"templates/pages/sidebar/sidebar-rigth-product.html"
 		};
-	}])
-	.directive('bannerAdvertising', [function () {
-		return {
-			restrict: 'E',
-			templateUrl:"templates/pages/banner/banner-advertising.html"
-		};
-	}])
-	.directive('sidebarCategory', [function () {
-		return {
-			restrict: 'A',
-			templateUrl:"templates/pages/sidebar/sidebar-category.html"
-		};
-	}])
+	}])	
 	.directive('mainFooter', [function () {
 		return {
 			restrict: 'A',
@@ -44,23 +32,6 @@
 			templateUrl:"templates/pages/copyright.html"
 		};
 	}])
-	.directive('owlItemBanner', [function () {
-		return {
-			restrict: 'E',
-			templateUrl:"templates/pages/content/owl-item-banner.html",
-			scope:{
-				desc:"@",
-			}
-		};
-	}])
-	.directive('owlItemProduct', [function () {
-		return {
-			restrict: 'E',
-			templateUrl:"templates/pages/content/owl-item-product.html",
-			scope:{
-			desc:"@"}
-		};
-	}])
 	.directive('contentSecundary', ["$http","$compile",function ($http,$compile) {
 		return {
 			restrict: 'E',
@@ -70,20 +41,46 @@
 				data:"@"
 			},
 			link:function(scope) {
-				$http.get("http://localhost/ApiZonngo/public/index.php/Categoria/1/Modelo").success(function(data){
-					data.forEach(function(i){
-						$(".owl-carousel-modelos").append($compile("<owl-item-product desc='"+i.desc+"''></owl-item-product>")(scope));
-					});
-					$(".owl-carousel-modelos").owlCarousel();
-				});
-				$http.get("http://localhost/ApiZonngo/public/index.php/Categoria").success(function(data){
+				var path="http://localhost/ApiZonngo/public/index.php";
+				var Categorias=scope.Categorias;
+				$http.get(path+"/Categoria").success(function(data){
 					data.forEach(function(i){
 						$(".owl-carousel-banner").append($compile("<owl-item-banner desc='"+i.desc+"'></owl-item-banner>")(scope));
 					});
-					$(".owl-carousel-banner").owlCarousel();				
+					$(".owl-carousel-banner").owlCarousel();	
+
+					$(".owl-carousel-modelos-title").text(data[0].desc);
+					data[0]["Marcas"].forEach(function(i){
+						i["Modelos"].forEach(function(j){
+							$(".owl-carousel-modelos").append($compile("<owl-item-product desc='"+j.desc+"''></owl-item-product>")(scope));	
+						});
+					});
+					$(".owl-carousel-modelos").owlCarousel();
 				});
-				
 			}
+		};
+	}])
+		.directive('owlItemBanner', [function () {
+			return {
+				restrict: 'E',
+				templateUrl:"templates/pages/content/owl-item-banner.html",
+				scope:{
+					desc:"@",
+				}
+			};
+		}])
+		.directive('owlItemProduct', [function () {
+			return {
+				restrict: 'E',
+				templateUrl:"templates/pages/content/owl-item-product.html",
+				scope:{
+				desc:"@"}
+			};
+		}])
+		.directive('bannerAdvertising', [function () {
+			return {
+				restrict: 'E',
+				templateUrl:"templates/pages/banner/banner-advertising.html"
 		};
 	}]);
 })();
